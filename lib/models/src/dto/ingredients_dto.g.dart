22e8a25edc644182640580f87d18ef4c8233e440
Rubber <rubberbigpepper@gmail.com>
2020-10-14 23:05:10 +0500

    урок 4 от OTUS - JSON, HTTP & serialization

---
+++ lib/models/src/dto/ingredients_dto.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'ingredients_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+IngredientsDto _$IngredientsDtoFromJson(Map<String, dynamic> json) {
+  return IngredientsDto(
+    json['idIngredient'] as String,
+    json['strIngredient'] as String,
+    json['strDescription'] as String,
+    json['strType'] as String,
+    json['strAlcohol'] as String,
+    json['strABV'] as String,
+  );
+}
